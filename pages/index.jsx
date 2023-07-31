13f186eeb6b2daa7fa4b5096e1ec8e3ccdabccbf
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-07-31 18:59:57 +0700

    20

--- pages/index.jsx
+++ pages/index.jsx
@@ -167,9 +167,9 @@ export default function Home() {
       <main className='' data-theme='sq'>
         <img
           className='fixed object-none object-left h-full'
-          src='img/coverbg.png'
+          src='img/coverbg.jpg'
         />
-        <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <div className='absolute top-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
           <img className='' src='img/SQ15-col.png' />/
         </div> 
         <div className='absolute'>
